115b66a720d1ebb7dfa4b65e196878dce7d04584
TatjanaPro <tatjaana.pronina@gmail.com>
2025-10-18 13:52:43 +0300

    config changes

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  ocument.getElementById('root')
+);
