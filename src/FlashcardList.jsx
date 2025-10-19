313161d5079df0902a80e9c4e7f398138d445b44
TatjanaPro <tatjaana.pronina@gmail.com>
2025-10-19 23:18:55 +0300

    finale css and rendering

--- src/FlashcardList.jsx
+++ src/FlashcardList.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Flashcard from './Flashcard';
 
+
 export default function FlashcardList({flashcards}) {
   return (
-    <div clasName="card-grid">
+    <div className="card-grid">
         {flashcards.map(flashcard => {
             return <Flashcard flashcard={flashcard} key ={flashcard.id} />
         })}
       
     </div>
-  )
+    
+  
+)
 }
